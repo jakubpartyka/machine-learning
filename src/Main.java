6f3cc89a16d70be4c8fe225577ab8b21b2ef1935
jakubpartyka <j.partyka20@gmail.com>
2020-03-26 19:40:27 +0100

    distance calculation implemented

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -9,6 +10,7 @@ public class Main {
     private static final String DATA_SET_FILENAME = "iris.data";
     private static int k = 4;
     private static List<TrainingObject> trainingSet = new ArrayList<>();
+    private static List<TrainingObject> dataSet = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Program start");
@@ -17,11 +19,43 @@ public class Main {
         //load training set from file
         loadTrainingSet();
 
+        //load actual data set
         loadSetToAnalyze();
 
+        analyzeData();
 
 
 
+
+
+
+    }
+
+    private static void analyzeData() {
+        for (TrainingObject dataObject : dataSet) {
+            HashMap<TrainingObject,Double> distances = new HashMap<>();
+
+            //calculate distances
+            for (TrainingObject trainingObject : trainingSet) {
+                distances.put(trainingObject,calculateDistance(dataObject,trainingObject));
+            }
+
+            //determine data object type according to K parameter
+
+
+
+        }
+
+    }
+
+    private static Double calculateDistance(TrainingObject dataObject, TrainingObject trainingObject) {
+        Double distance;
+        double A = dataObject.a, B = dataObject.b, C = dataObject.c, D = dataObject.d;
+        double a = trainingObject.a, b = trainingObject.b, c = trainingObject.c, d = trainingObject.d;
+
+        distance = Math.sqrt(Math.pow(a-A,2) + Math.pow(b-B,2) + Math.pow(c-C,2) + Math.pow(d-D,2));
+
+        return distance;
     }
 
     private static void loadTrainingSet() {
@@ -54,10 +88,10 @@ public class Main {
                 String [] data = line.split(",");
 
                 AnalyzedObject object = new AnalyzedObject(data[0],data[1],data[2],data[3],data[4]);
-                trainingSet.add(object);
+                dataSet.add(object);
                 line = reader.readLine();
             }
-            System.out.println("training set was read successfully");
+            System.out.println("data set was read successfully");
 
         } catch (IOException e) {
             e.printStackTrace();
